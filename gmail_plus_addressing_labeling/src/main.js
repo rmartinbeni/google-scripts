47413417f4224221e7a2189d98e7388298ca2bc5
Ricard Martin Benito <32068910+rmartinbeni@users.noreply.github.com>
2024-08-28 17:04:43 +0200

    gmail: rename app scripts

--- gmail_plus_addressing_labeling/src/main.js
+++ gmail_plus_addressing_labeling/src/main.js
@@ -1,15 +1,3 @@
-const tagEmail = () => {
-  let start = 0
-  let inboxMails = null
-  const tags = getTags()
-
-  do {
-    inboxMails = GmailApp.search(INBOX_QUERY, start, PAGE_SIZE)
-    inboxMails.forEach((thread) => processThread(thread, tags))
-    start += PAGE_SIZE
-  } while (inboxMails.length === PAGE_SIZE)
-}
-
 const main = () => {
   tagEmail()
 }
